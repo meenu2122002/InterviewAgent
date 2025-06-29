4d522d5b772d3132c7df336847fbafae2c7e967e
Meenu <techexploread2@gmail.com>
2025-06-29 18:16:18 +0530

    comiteding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,7 @@ const RootLayout = async({ children }: { children: ReactNode }) => {
         <Link href="/" className='flex items-center gap-2'>
           <Image src="/logo.svg" alt="logo" width={38} height={32} />
           <h2 className='text-primary-100'>
-            PrepWise
+            WiseMock
           </h2>
         </Link>
       </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const monaSans = Mona_Sans({
 
 
 export const metadata: Metadata = {
-  title: "Prepwise",
+  title: "wiseMock",
   description: "AI Agent for interview generation",
 };
 
